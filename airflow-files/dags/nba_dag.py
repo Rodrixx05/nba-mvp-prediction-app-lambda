534511839ac1d8cb204b750358e271b6a9ebd480
= <marc.rodri5#@gmail.com>
2022-09-02 00:49:34 +0200

    Stuck at running airflow scheduler for DAG appearing in UI. Creating new branch

---
+++ airflow-files/dags/nba_dag.py
@@ -0,0 +1,19 @@
+from airflow.decorators import task, dag
+from datetime import datetime
+import basketball_reference_rodrixx as brr
+
+@dag(
+    description='DAG which imports data daily from Basketball Reference and makes predictions on the MVP winner',
+    schedule_interval='*/5 * * * *',
+    start_date=datetime(2022, 8, 1),
+    catchup=False,
+    tags=['nba'],
+)
+def nba_mvp_predictor():
+    @task(task_id = 'import_data')
+    def import_data_br(year = 2023, path = '/opt/bitnami/pickles'):
+        getter = brr.BasketballReferenceGetter()
+        br_df = getter.extract_player_stats_multiple(year, mvp = False, advanced = True, ranks = True)
+        br_df.to_pickle(path + '/raw_df.pkl')
+
+predict_nba_mvp = nba_mvp_predictor()
--- airflow-files/my-py-files/data_acquisition.py
+++
@@ -1,6 +0,0 @@
-import basketball_reference_rodrixx as brr
-
-def import_data_br(year = 2023, path = '/opt/bitnami/pickles'):
-    getter = brr.BasketballReferenceGetter()
-    br_df = getter.extract_player_stats_multiple(year, mvp = False, advanced = True, ranks = True)
-    br_df.to_pickle(path + '/raw_df.pkl')
