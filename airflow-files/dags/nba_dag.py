4ee0439f1ec5f1d8e39ebf3444ea97bbcbd2f28d
= <marc.rodri5#@gmail.com>
2022-09-02 13:00:49 +0200

    Added second task in the dag, consisting in the preprocessing of the dataframe for passing it to the ML model

--- airflow-files/dags/nba_dag.py
+++ airflow-files/dags/nba_dag.py
@@ -1,6 +1,15 @@
 from airflow.decorators import task, dag
 from datetime import datetime
+import os
+
+import pandas as pd
+from sklearn.pipeline import Pipeline
+
 import utils.basketball_reference_rodrixx as brr
+import utils.preprocessing_lib_rodrixx as prep
+
+
+DATA_PATH = '/opt/airflow/data'
 
 @dag(
     description='DAG which imports data daily from Basketball Reference and makes predictions on the MVP winner',
@@ -9,14 +18,44 @@ import utils.basketball_reference_rodrixx as brr
     catchup=False,
     tags=['nba'],
 )
+
 def nba_mvp_predictor():
-    @task(task_id = 'import_data')
-    def import_data_br(year = 2022, path = '/opt/airflow/data'):
+
+    @task()
+
+    def import_data_br(year = 2022, file_out = 'raw_df.pkl', path_out = DATA_PATH):
+
         getter = brr.BasketballReferenceGetter()
-        br_df = getter.extract_player_stats_multiple(year, mvp = False, advanced = True, ranks = True)
-        br_df.to_pickle(path + '/raw_df.pkl')
+        raw_df = getter.extract_player_stats_multiple(year, mvp = False, advanced = True, ranks = True)
+
+        df_file = os.path.join(path_out, file_out)
+        raw_df.to_pickle(df_file)
+    
+    @task()
+    
+    def preprocess_data(file_in = 'raw_df.pkl', path_in = DATA_PATH, file_out = 'pre_df.pkl', path_out = DATA_PATH):
+
+        df_file_in = os.path.join(path_in, file_in)
+        pre_df = pd.read_pickle(df_file_in)
+
+        cols_to_drop = ['Rk', 'GT', 'FG_tot', '3PA_tot', '2PA_tot', 'FGA_rank_tot', 'Tm', 'Pos']
+        cols_to_filter = ['PER', 'WS/48', 'BPM', 'USG%']
+
+        pipe_prep = Pipeline(steps = [
+            ('DropPlayersMultiTeams', prep.DropPlayersMultiTeams()),
+            ('OutlierFilter', prep.OutlierFilter(q = .0005, col_to_filter = cols_to_filter)),
+            ('SetIndex', prep.SetIndex()),
+            ('DropColumns', prep.DropColumns(cols_to_drop)),
+            ('DropPlayers', prep.DropPlayers()),
+        ])
+        pre_df = pipe_prep.fit_transform(pre_df)
+
+        df_file_out = os.path.join(path_out, file_out)
+        pre_df.to_pickle(df_file_out)
     
     data_import = import_data_br()
+    data_preprocessed = preprocess_data()
 
+    data_import >> data_preprocessed
 
 predict_nba_mvp = nba_mvp_predictor()
