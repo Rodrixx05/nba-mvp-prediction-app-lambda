1c39bf292ac1a47b1604f83eca3c65cd22f6a519
= <marc.rodri5#@gmail.com>
2022-09-02 02:49:42 +0200

    First dag executed correctly importing data from basketball reference

--- airflow-files/dags/nba_dag.py
+++ airflow-files/dags/nba_dag.py
@@ -1,6 +1,6 @@
 from airflow.decorators import task, dag
 from datetime import datetime
-import basketball_reference_rodrixx as brr
+import utils.basketball_reference_rodrixx as brr
 
 @dag(
     description='DAG which imports data daily from Basketball Reference and makes predictions on the MVP winner',
@@ -11,9 +11,12 @@ import basketball_reference_rodrixx as brr
 )
 def nba_mvp_predictor():
     @task(task_id = 'import_data')
-    def import_data_br(year = 2023, path = '/opt/bitnami/pickles'):
+    def import_data_br(year = 2022, path = '/opt/airflow/data'):
         getter = brr.BasketballReferenceGetter()
         br_df = getter.extract_player_stats_multiple(year, mvp = False, advanced = True, ranks = True)
         br_df.to_pickle(path + '/raw_df.pkl')
+    
+    data_import = import_data_br()
+
 
 predict_nba_mvp = nba_mvp_predictor()
