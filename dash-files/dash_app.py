3e6d804cd823e3ed11e5f40607fe5083996e0f44
= <marc.rodri5#@gmail.com>
2022-09-26 18:43:59 +0200

    Timeseries graph with all configuration options developed

--- dash-files/dash_app.py
+++ dash-files/dash_app.py
@@ -6,16 +6,15 @@ from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import plotly.graph_objects as go
+import plotly.io as pio
 
-import os
 import re
 
 import utils.dashboard_lib_rodrixx as dlib
 
-# import sys 
-# sys.path.append(os.getcwd())
-
 db_table_name = 'stats_predictions'
+results_labels = {'PREDSHARE': 'Predicted Share', 'PREDVOTES': 'Predicted Votes'}
+# pio.templates.default = 'ggplot2'
 
 conn_url = 'postgresql://Rodrixx:Jordan-23@localhost:5432/nba_db'
 engine = create_engine(conn_url)
@@ -27,13 +26,21 @@ query_columns = f"""
 query_players = f"""
     SELECT DISTINCT "PLAYER" FROM {db_table_name};
 """
+query_datetime = f"""
+    SELECT max("DATETIME"), min("DATETIME") FROM {db_table_name}
+"""
+
 with engine.connect() as con:
     result_columns = con.execute(query_columns)
     result_players = con.execute(query_players)
+    result_datetime = con.execute(query_datetime)
 
 columns_list = [element[0] for element in result_columns]
 players_list = [element[0] for element in result_players]
 players_list.sort(key = lambda x: x.split(' ')[1])
+tuple_datetime = list(result_datetime)[0]
+max_datetime = tuple_datetime[0]
+min_datetime = tuple_datetime[1]
 
 models_list = [re.search('^PREDVOTES_(.+)', element).group(1) for element in columns_list if re.search('^PREDVOTES', element)]
 no_stats_list = ['SEASON', 'RK', 'PLAYER', 'AGE', 'TM', 'POS']
@@ -50,13 +57,13 @@ app.layout = html.Div(
         dbc.Row(dbc.Col(html.P('Fiddle with all the options and have fun!', style={'textAlign': 'center'}), width = 10), justify = 'center'),
         dbc.Container(dbc.Card(
             [
-                dbc.CardHeader(html.H4('Initial configuration', className = 'card_title', style={'textAlign': 'center'})),
+                dbc.CardHeader(html.H3('Initial configuration', className = 'card_title', style={'textAlign': 'center'})),
                 dbc.CardBody(dbc.Container(
                 [
                     dbc.Row(
                         [
-                            dbc.Col(html.H3('Select players', style={'textAlign': 'center'}), width = 6),
-                            dbc.Col(html.H3('Select model', style={'textAlign': 'center'}), width = 6)
+                            dbc.Col(html.H4('Select players', style={'textAlign': 'center'}), width = 6),
+                            dbc.Col(html.H4('Select model', style={'textAlign': 'center'}), width = 6)
                         ], justify = 'center'),
                     dbc.Row(
                         [
@@ -65,8 +72,8 @@ app.layout = html.Div(
                                 [
                                     html.Div(children = 
                                         [
-                                            dcc.Input(id = 'number_players', type = 'number', min = 2, max = 10, step = 1, placeholder = 'Nº Players', style={'width': 150}, value = 3)
-                                        ], id = 'container_best_players', style = {'display': 'block'}),
+                                            dcc.Input(id = 'number_players', type = 'number', min = 2, max = 10, step = 1, placeholder = 'Nº Players', style={'width': 100}, value = 3)
+                                        ], id = 'container_best_players', style = {'display': 'flex', 'justify-content': 'center'}),
                                     html.Div(children = 
                                         [
                                             dcc.Dropdown(id = 'dropdown_players', options = players_list, placeholder = 'Select players', multi = True)
@@ -76,12 +83,25 @@ app.layout = html.Div(
                             dbc.Col(width = 1)
                         ], align = 'center', justify = 'center')
                 ]))
-            ], color = 'secondary', inverse = False)),
-        dbc.Row(dbc.Col(html.H3('MVP score timeseries'), style={'margin-top': 30, 'textAlign': 'center'})),
-        dbc.Row(
+            ])),
+        dbc.Container(dbc.Card(
             [
-                dbc.Col(dcc.Graph(id = 'graph_timeseries', figure = {}))
-            ])
+                dbc.CardHeader(html.H3('MVP score timeseries', className = 'card_title', style={'textAlign': 'center'})),
+                dbc.CardBody(dcc.Graph(id = 'graph_timeseries', figure = {})),
+                dbc.CardFooter(dbc.Row(
+                    [
+                        dbc.Col(
+                            [
+                                html.H5('Select desired interval', style={'textAlign': 'center'}),
+                                dcc.DatePickerRange(id = 'daterange_timeseries', min_date_allowed = min_datetime, max_date_allowed = max_datetime, initial_visible_month = max_datetime, start_date = min_datetime, end_date = max_datetime),
+                            ], width = 'auto'),
+                        dbc.Col(
+                            [
+                                html.H5('Choose model output', style={'textAlign': 'center'}),
+                                dcc.RadioItems(id = 'radio_value_timeseries', options = results_labels, value = 'PREDSHARE', inputStyle={"margin-left": "8px", "margin-right": "5px"})
+                            ], width = 'auto')
+                    ], align = 'center', justify = 'evenly'))
+            ]), style={"margin-top": "25px"})
     ])
 
 @app.callback(
@@ -90,49 +110,52 @@ app.layout = html.Div(
     Input('radio_select_players', 'value')
 )
 def update_select_players(option):
-    show = {'display': 'block'}
+    show_best = {'display': 'flex', 'justify-content': 'center'}
+    show_custom = {'display': 'block'}
     hide = {'display': 'none'}
     if option == 'Best players':
-        return show, hide
+        return show_best, hide
     else:
-        return hide, show
+        return hide, show_custom
 
 @app.callback(
     Output('graph_timeseries', 'figure'),
     Input('radio_select_players', 'value'),
     Input('number_players', 'value'),
     Input('dropdown_players', 'value'),
-    Input('dropdown_model', 'value')
+    Input('dropdown_model', 'value'),
+    Input('daterange_timeseries', 'start_date'),
+    Input('daterange_timeseries', 'end_date'),
+    Input('radio_value_timeseries', 'value')
 )
-def update_timeseries(option, number, players, model):
+def update_timeseries(option, number, players, model, start_date, end_date, value):
     if option == 'Best players':
         query_timeseries = f"""
-            SELECT "DATETIME", "PLAYER", "PREDSHARE_{model}" FROM stats_predictions
+            SELECT "DATETIME", "PLAYER", "{value}_{model}" FROM {db_table_name}
             WHERE "PLAYER" IN (
                 SELECT "PLAYER" FROM (
-                    SELECT "PLAYER", "DATETIME", "PREDSHARE_{model}" FROM stats_predictions
-                    WHERE "DATETIME" IN (SELECT max("DATETIME") FROM stats_predictions)
-                    ORDER BY "PREDSHARE_{model}" DESC
+                    SELECT "PLAYER", "DATETIME", "{value}_{model}" FROM {db_table_name}
+                    WHERE "DATETIME" IN (SELECT max("DATETIME") FROM {db_table_name})
+                    ORDER BY "{value}_{model}" DESC
                     LIMIT {number}
                 ) AS best_players 
-            )
+            ) AND "DATETIME" BETWEEN '{start_date}' AND '{end_date}'
             ORDER BY "DATETIME" DESC;
         """
         timeseries_df = pd.read_sql(query_timeseries, engine, index_col = "DATETIME", parse_dates = ["DATETIME"])
-        fig_timeseries = px.line(data_frame = timeseries_df, y = f'PREDSHARE_{model}', color = 'PLAYER')
-        fig_timeseries.update_layout(yaxis_title = 'Voting Share', xaxis_title = 'Date', legend_title = 'Players')
-        fig_timeseries.update_xaxes(rangeslider_visible=True)
+        fig_timeseries = px.line(data_frame = timeseries_df, y = f'{value}_{model}', color = 'PLAYER')
+        fig_timeseries.update_layout(yaxis_title = results_labels[value], xaxis_title = 'Date', legend_title = 'Players', margin=dict(l=20, r=20, t=20, b=20))
         return fig_timeseries
     else:
         query_timeseries = f"""
-            SELECT "DATETIME", "PLAYER", "PREDSHARE_{model}" FROM stats_predictions
-            WHERE "PLAYER" IN {tuple(players)}
+            SELECT "DATETIME", "PLAYER", "{value}_{model}" FROM {db_table_name}
+            WHERE "PLAYER" IN ({str(players)[1:-1]})
+            AND "DATETIME" BETWEEN '{start_date}' AND '{end_date}'
             ORDER BY "DATETIME" DESC;
         """
         timeseries_df = pd.read_sql(query_timeseries, engine, index_col = "DATETIME", parse_dates = ["DATETIME"])
-        fig_timeseries = px.line(data_frame = timeseries_df, y = f'PREDSHARE_{model}', color = 'PLAYER')
-        fig_timeseries.update_layout(yaxis_title = 'Voting Share', xaxis_title = 'Date', legend_title = 'Players')
-        fig_timeseries.update_xaxes(rangeslider_visible=True)
+        fig_timeseries = px.line(data_frame = timeseries_df, y = f'{value}_{model}', color = 'PLAYER')
+        fig_timeseries.update_layout(yaxis_title = results_labels[value], xaxis_title = 'Date', legend_title = 'Players')
         return fig_timeseries
 
 if __name__ == '__main__':
